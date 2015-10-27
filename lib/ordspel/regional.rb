2b0aeb199bab032f2b1c34ee7b415539d6733de1
Rickard Sunden <rickard.sunden@outlook.com>
2015-10-26 22:34:39 -0400

    Add minnesotafy method. :)

--- lib/ordspel/regional.rb
+++ lib/ordspel/regional.rb
@@ -6,5 +6,20 @@ module Ordspel
       self.gsub(/Er|Ar/, "Ah").gsub(/er|ar/, "ah")
     end
 
+    def minnesotafy
+      counter = 0
+      string = self.split(/(?<=[?\.!])\s/)
+      string.map do |sentence|
+        sentence = sentence + " "
+        ending = sentence.scan(/[?\.!]\s/).first
+        counter += 1
+        if counter < string.length
+          "Oh ya " + sentence.gsub(ending,'') + " you bet ya" + ending
+        else
+          "Oh ya " + sentence.gsub(ending,'') + " you bet ya" + ending[0..-2]
+        end
+      end.join
+    end
+
   end
 end
--- spec/regional_spec.rb
+++ spec/regional_spec.rb
@@ -7,4 +7,13 @@ RSpec.describe Ordspel::Language do
     expect(string.bostonify).to eq("I pahked my cah at Hahvahd yahd.")
   end
 
+  it 'minnesotafy should return the right string' do
+    string = "I parked my car at Harvard yard."
+    expect(string.minnesotafy).to eq("Oh ya I parked my car at Harvard yard you bet ya.")
+    string = "My mom loves cheese. She really does. Yep she really does."
+    expect(string.minnesotafy).to eq("Oh ya My mom loves cheese you bet ya. Oh ya She really does you bet ya. Oh ya Yep she really does you bet ya.")
+    string = "This.is a crazy test."
+    expect(string.minnesotafy).to eq("Oh ya This.is a crazy test you bet ya.")
+    end
+
 end
